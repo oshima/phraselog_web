26e72ac21643e7790f98844cb6b5964035aca849
oshima <k.oshjma@gmail.com>
2019-03-03 19:12:10 +0900

    Change logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import blue from '@material-ui/core/colors/blue';
+import teal from '@material-ui/core/colors/teal';
 import lime from '@material-ui/core/colors/lime';
 
 const Root = styled.svg`
@@ -11,9 +11,9 @@ const Root = styled.svg`
 function Logo() {
   return (
     <Root width={20} height={20}>
-      <rect x={0} y={0} width={10} height={10} fill={lime[200]} />
+      <rect x={0} y={0} width={10} height={10} fill={lime[300]} />
       <rect x={10} y={0} width={10} height={10} fill={lime[600]} />
-      <rect x={0} y={10} width={10} height={10} fill={blue[500]} />
+      <rect x={0} y={10} width={10} height={10} fill={teal[400]} />
     </Root>
   );
 }
